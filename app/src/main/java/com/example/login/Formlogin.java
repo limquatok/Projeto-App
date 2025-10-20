b4eb262d2a300241b9e0233e79590074b9c7eddd
limquatok <isaquemaropo@gmail.com>
2025-10-20 13:48:51 -0300

    Login e Cadastro

---
+++ app/src/main/java/com/example/login/Formlogin.java
@@ -0,0 +1,45 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Formlogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_fomlogin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Formlogin.this,FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+    }
+
+}
